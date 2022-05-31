e61e17ce0e5e0507a5f55d937ab643b29f8e4b83
Davi Reis <reis.davi11010@gmail.com>
2022-05-31 17:02:39 -0300

    added gate script to House Project

---
+++ Arduino/House/gate/gate.ino
@@ -0,0 +1,29 @@
+// Including Servo library
+#include <Servo.h>
+
+// Defining components PINs and gate states
+#define PIN_BT 12
+#define PIN_SERVO A0
+#define OPEN 1
+#define CLOSED 0
+
+// State variable and servo object
+bool gateState = CLOSED;
+Servo servo;
+
+void setup()
+{
+  pinMode(PIN_BT, INPUT_PULLUP);
+  servo.attach(PIN_SERVO);
+}
+
+void loop()
+{
+  if (digitalRead(PIN_BT) == LOW)
+  {
+    gateState = (gateState == OPEN) ? CLOSED : OPEN;
+  }
+
+  servo.write(100 * gateState);
+  delay(100);
+}
